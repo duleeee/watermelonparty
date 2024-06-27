f276089d132e5d8ce5efc48c6c9b1533be17ef95
duleeee <duleee@gmail.com>
2024-06-27 13:22:31 +0200

    Update ParticipantDetails.js

--- src/pages/ParticipantDetails.js
+++ src/pages/ParticipantDetails.js
@@ -92,8 +92,9 @@ const ParticipantDetails = () => {
       {step === 4 && (
         <div className="content">
           <h2>Please proceed to the Front Desk to make the payment.</h2>
-        <p><b>The price is 10 euros per person and we accept only cash.</b> </p> 
-        <p>Show this screen to the front desk person.</p>
+        <p><b>The price is 9.99 euros per person. </p>
+        <p>We accept cash only.</b> </p> 
+                <p>Show this screen to the front desk person.</p>
           <p><strong>Name:</strong> {participant.name}</p>
           <p><strong>Email:</strong> {participant.email}</p>
           <p><strong>Drinking Name:</strong> {participant.drinkingName}</p>
