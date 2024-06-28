055705e12cdb30967925a494adb8674c58d1160b
duleeee <duleee@gmail.com>
2024-06-28 14:30:01 +0200

    Update ParticipantDetails.js

--- src/pages/ParticipantDetails.js
+++ src/pages/ParticipantDetails.js
@@ -15,10 +15,7 @@ const ParticipantDetails = () => {
     setParticipant(prev => ({ ...prev, [name]: value }));
   };
 
-  const copyEmail = () => {
-    var email = "watermelonpartysplit@gmail.com"; // Replace with your email address
-    navigator.clipboard.writeText(email)
-}
+
 
   
   const validateEmail = (email) => {
@@ -107,9 +104,7 @@ const ParticipantDetails = () => {
           <p><strong>Confirmation Code:</strong> {confirmation}</p>
         <p>If you were signed up on a beach, please send this screenshot to:</p>
         <p><strong> WATERMELONPARTYSPLIT@GMAIL.COM </strong></p>
-        <div> className="button-container">
-      <button className="selection-button" onclick="copyEmail()">Copy Email</button>
-        </div>
+    
         <p>You will receive the confirmation email shortly after the payment to the promoter.</p>
           <div className="button-container">
             <button className="selection-button" onClick={() => window.location.href = 'https://www.google.com'}>Finish</button>
